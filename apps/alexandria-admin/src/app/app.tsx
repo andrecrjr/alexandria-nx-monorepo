ad5d33e50b3c1ae9eb2f479c6a44fee2c2e10be7
André Carlos Jr <7965520+andrecrjr@users.noreply.github.com>
2024-06-25 23:08:39 -0300

    feature: add shadcn ui to nx

--- apps/alexandria-admin/src/app/app.tsx
+++ apps/alexandria-admin/src/app/app.tsx
@@ -1,28 +1,12 @@
 import NxWelcome from './nx-welcome';
 
 import { Route, Routes, Link } from 'react-router-dom';
+import {Button} from "@alexandria/shadcn-ui/components/ui/button"
+import {Label} from "@alexandria/shadcn-ui/components/ui/label"
+
 
 export function App() {
   return (
-    <div>
-      <NxWelcome title="alexandria-admin" />
-
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
-      </div>
       <Routes>
         <Route
           path="/"
@@ -42,8 +26,6 @@ export function App() {
           }
         />
       </Routes>
-      {/* END: routes */}
-    </div>
   );
 }
 
